e793f6a03e563cb5cbd78ce1b536a6c131a69ec9
runescape <henkka008@hotmail.com>
2025-11-06 11:30:34 +0200

    added ingame fps

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -19,6 +23,7 @@ def main():
 
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
