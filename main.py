90e65bdf318bfb6a7c842054c87b187a37afe718
henkka008 <henkka008@hotmail.com>
2025-11-06 15:38:04 +0200

    Added groups to the main.py after smashing head against the wall many times.

--- main.py
+++ main.py
@@ -12,23 +12,32 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
+    dt = 0
+
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        updatable.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
-        p1.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
-        p1.update(dt)
+        for x in drawable:
+            x.draw(screen)
 
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000 #fps limit
 
 
 if __name__ == "__main__":
